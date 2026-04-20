f1405564b1c3548d28db8968604788b16ec0cde3
student <student@ubuntu.myguest.virtualbox.org>
2026-04-20 08:52:44 +0200

    Changed robot visualization model to TB3

--- src/unicycle_description/launch/display.launch.py
+++ src/unicycle_description/launch/display.launch.py
@@ -8,10 +8,12 @@ def generate_launch_description():
     package_name = 'unicycle_description'
     pkg_share = get_package_share_directory(package_name)
 
-    xacro_file = os.path.join(pkg_share, 'urdf', 'unicycle.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
-    robot_desc = robot_description_config.toxml()
+    tb3_share = get_package_share_directory('turtlebot3_description')
+    xacro_file = os.path.join(tb3_share, 'urdf', 'turtlebot3_burger.urdf')
 
+    robot_description_config = xacro.process_file(xacro_file, mappings={'robot_namespace': ''})
+    robot_desc = robot_description_config.toxml()
+    
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -26,13 +28,20 @@ def generate_launch_description():
         arguments=['-d', os.path.join(pkg_share, 'rviz', 'simulation.rviz')]
     )
 
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher'
+    )
+
     return LaunchDescription([
         robot_state_publisher_node,
+        joint_state_publisher_node,
         rviz_node,
         # Dodajemy statyczne połączenie odom -> base_link, żeby RViz wiedział gdzie jest robot
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
-            arguments=['0', '0', '0', '0', '0', '0', 'odom', 'base_link']
+            arguments=['0', '0', '0', '0', '0', '0', 'odom', 'base_footprint']
         )
     ])
